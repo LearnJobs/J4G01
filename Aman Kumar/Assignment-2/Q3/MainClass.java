3c85f89e3e9d670e26efefb0634bd2bf487517e3
Amankr0007 <96586209+Amankr0007@users.noreply.github.com>
2022-03-16 16:12:31 +0530

    Add files via upload

---
+++ Aman Kumar/Assignment-2/Q3/MainClass.java
@@ -0,0 +1,10 @@
+package Q3;
+
+public class MainClass {
+	public static void main(String args[])
+	{
+		UpperAndLowerCase upr = new UpperAndLowerCase();
+		upr.upperCase();
+		upr.lowerCase();
+	}
+}
---
+++ Aman Kumar/Assignment-2/Q3/UpperAndLowerCase.java
@@ -0,0 +1,17 @@
+package Q3;
+
+public class UpperAndLowerCase {
+	String s1="aBhaY";
+	String s2="AjaY";
+	public void upperCase()
+	{
+		System.out.println(s1.toUpperCase());
+		System.out.println(s2.toUpperCase());
+	}
+	public void lowerCase()
+	{
+		System.out.println(s1.toLowerCase());
+		System.out.println(s2.toLowerCase());
+	}
+
+}
