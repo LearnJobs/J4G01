2f215ca7c2dc1f0432635623abf1ab5081434732
Rajeshwari05 <101709831+Rajeshwari05@users.noreply.github.com>
2022-03-17 00:40:47 +0530

    Add files via upload

---
+++ DemoName.java
@@ -0,0 +1,11 @@
+
+public class DemoName {
+	static String mystring = "Rajeshwari";
+	
+	int mystringlength = mystring.length();
+	
+	public static void Namedisplay() {
+		System.out.println(mystring);
+	}
+	
+}
