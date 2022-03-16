97577ca676a9dc41ebf6ffe4db302229f3c507ab
anangidivya <101708785+anangidivya@users.noreply.github.com>
2022-03-16 16:56:24 +0530

    Add files via upload

---
+++ Anangi Divya/Assignment-01/MyName.java
@@ -0,0 +1,15 @@
+package module;
+
+public class MyName {
+	static void myMethod() 
+	{
+		System.out.println("My name is Divya");
+	}
+	public static void main(String[] args) {
+		MyName.myMethod();
+			
+		}
+
+	}
+
+
