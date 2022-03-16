9047e8f01823566e598e765748e62d6c7ef58d4d
211311998 <101707619+211311998@users.noreply.github.com>
2022-03-16 22:04:00 +0530

    Add files via upload

---
+++ Uma Gangadhari/Assignment-01/SumAndAverage.java
@@ -0,0 +1,26 @@
+
+public class SumAndAverage {
+	
+		static int sum1=0;
+
+		public static void main(String[] args) {
+			int[] a=new int[] {10,20,30,40,50};
+			SumAndAverage a1=new SumAndAverage();
+			for (int i=0;i<a.length;i++)
+			a1.sum(a[i]);
+			System.out.println("Sum="+sum1);
+			a1.average(a.length);
+		}
+	public void sum(int s)
+	{
+	sum1=sum1+s;
+	}
+	public void average(int length)
+	{
+	int average=0;
+	average=sum1/length;
+	System.out.println("average"+average);
+	}
+	}
+
+
