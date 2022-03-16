fc69d37df411aef03a051e6eebfb3ff6c94f6efe
211311998 <101707619+211311998@users.noreply.github.com>
2022-03-16 17:17:49 +0530

    Add files via upload

---
+++ Uma Gangadhari/Assignment-01/Book.java
@@ -0,0 +1,23 @@
+
+public class Book {
+	int bookId;
+	String bookName;
+	String bookAuthor;
+
+	public static void main(String[] args) {
+		Book book=new Book();
+		book.printBook();
+		
+	}
+	Book()
+	{
+		bookId=1;
+		bookName="Computer";
+		bookAuthor="Uma";
+
+	}
+public void printBook()
+{
+	System.out.println("Book Details Are="+bookId+" ,"+bookName+","+bookAuthor);
+}
+}
