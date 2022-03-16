277fda4e9d7dce5acfd2c745273649b1141988f1
ValluriDeepa <101707652+ValluriDeepa@users.noreply.github.com>
2022-03-16 16:17:28 +0530

    Add files via upload

---
+++ Valluri Deepa/Assignment-01/PrintMyName.java
@@ -0,0 +1,14 @@
+//module1-1
+public class PrintMyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Deepa");
+	}
+
+	public static void main1(String[] args) {
+		display();
+	}
+
+	
+}
