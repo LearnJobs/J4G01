547134b6998105bffe5f320c054f533e189ef1bb
ValluriDeepa <101707652+ValluriDeepa@users.noreply.github.com>
2022-03-16 16:40:43 +0530

    Add files via upload

---
+++ Valluri Deepa/Assignment-01/BookParameterized.java
@@ -0,0 +1,29 @@
+// module1-3
+public class BookParameterized {
+
+	// public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	int bookid;
+	String bookName;
+	String bookAuthor;
+
+	public BookParameterized(int bookid, String bookName, String bookAuthor) {
+		// super();
+		this.bookid = bookid;
+		this.bookName = bookName;
+		this.bookAuthor = bookAuthor;
+	}
+
+	void printBook() {
+		System.out.println("bookid is : " + bookid);
+		System.out.println("bookName is : " + bookName);
+		System.out.println("bookAuthor is : " + bookAuthor);
+	}
+
+	public static void main(String[] args) {
+
+		BookParameterized book = new BookParameterized(101, "Prince of Persia", "Shylock");
+		book.printBook();
+	}
+
+}
