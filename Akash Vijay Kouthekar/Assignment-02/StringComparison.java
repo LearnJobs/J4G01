02097cd2908058ba03a8f010dbb3a9f73850a6c9
Akash Kouthekar <kouthekar.ak47@gmail.com>
2022-03-16 08:55:43 +0530

    Create StringComparison.java

---
+++ Akash Vijay Kouthekar/Assignment-02/StringComparison.java
@@ -0,0 +1,44 @@
+/* Program 01 : Write a program to compare two strings with equals(),equalsIgnoreCase(),
+				compareTo(), endsWith(), startsWith()
+*/
+
+package assignment02;
+public class StringComparison implements Comparable<String> {
+
+	public static void main(String[] args) {
+		String string1 = "Akash Kouthekar";
+		String string2 = "Akash kouthekar";
+		
+		if(string1.equals(string2))
+			System.out.println("Equal");
+		else
+			System.out.println("Not equal");
+		
+		if(string1.equalsIgnoreCase(string2))
+			System.out.println("Equal");
+		else
+			System.out.println("Not equal");
+		
+		int result= string1.compareTo(string2);
+		if(result==0)
+			System.out.println("Equal");
+		else
+			System.out.println("Not equal");
+		
+		if(string1.startsWith("Akash"))
+			System.out.println("True");
+		else
+			System.out.println("False");
+		
+		if(string1.endsWith("Kouthekar"))
+			System.out.println("True");
+		else
+			System.out.println("False");
+	}
+
+	@Override
+	public int compareTo(String o) {
+		return this.compareTo(o);
+	}
+
+}
