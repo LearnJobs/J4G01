c286de9abe5b19132d9fc45d21c6a909f23c7329
Bhanusha-NS <101661869+Bhanusha-NS@users.noreply.github.com>
2022-03-16 16:35:10 +0530

    Add files via upload

---
+++ Bhanusha N S/Assignment-01/Books.java
@@ -0,0 +1,15 @@
+package com.assignments01;
+
+public class Books  {
+
+	public Books(String bookname, String bookauthor) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BookArray bookarray=new BookArray();
+		bookarray.details();
+	}
+
+}
