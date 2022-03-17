209cb1988be3c281aa8de11a23346e715638bf54
NarayanaRao443 <101687702+NarayanaRao443@users.noreply.github.com>
2022-03-17 11:04:06 +0530

    Add files via upload

---
+++ Kimidi Narayana Rao/Assignment-01/MyName1.java
@@ -0,0 +1,13 @@
+public class MyName1
+{
+	static void printName(String x)
+	{
+		System.out.println("My Name is: "+x);
+	}
+	
+	public static void main(String args[])
+	{
+		String s="Narayana Rao";
+		printName(s);
+	}
+}
