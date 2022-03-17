ed1c8296734a2faa8070038ed74c485b86c1c80d
anangidivya <101708785+anangidivya@users.noreply.github.com>
2022-03-17 21:13:51 +0530

    Add files via upload

---
+++ Anangi Divya/Assignment-01/Book.java
@@ -0,0 +1,20 @@
+package module;
+
+public class Book {
+	
+
+static int id;   
+
+    public String name;
+
+   abstract Book(int id;String name);
+    public static void main(String[] args)
+    {
+    	Object author;
+		Book().obj=new Book(id,author);
+    	
+    	System.out.println(101);
+    	System.out.println("abc");
+    }
+}
+        
