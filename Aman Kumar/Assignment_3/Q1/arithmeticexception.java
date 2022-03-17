7ffd06708a1caba8a9ca2ca5cd2c14db49ea77db
Amankr0007 <96586209+Amankr0007@users.noreply.github.com>
2022-03-17 21:38:37 +0530

    Add files via upload

---
+++ Aman Kumar/Assignment_3/Q1/arithmeticexception.java
@@ -0,0 +1,25 @@
+package Q1;
+
+public class arithmeticexception {
+	int x=15,y=5;
+	public void arithmeticException()
+	{
+		try {
+			int num=x/y;
+			System.out.println(num);
+		}
+		catch(ArithmeticException arthemeticexception) 
+		{
+			System.out.println("the number not divide by zero");
+		}
+		
+	}
+	
+	
+	public static void main(String args[])
+	{
+		arithmeticexception aE = new arithmeticexception();
+		aE.arithmeticException();
+	}
+
+}
