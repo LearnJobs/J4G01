cfe9baac6b24912263a49fb1ff1d9a9116fe665f
Nikki-61 <101709424+Nikki-61@users.noreply.github.com>
2022-03-17 17:00:18 +0530

    Add files via upload

---
+++ Mannam Nikitha/module01/PrintName.java
@@ -0,0 +1,11 @@
+package assignment01;
+
+public class PrintName {
+	public static void main(String[] args) {
+		printName();
+	}
+	public static void printName()
+	{
+		System.out.println("NIKKI");
+	}
+}
