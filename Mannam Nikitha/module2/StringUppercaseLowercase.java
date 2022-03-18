f16ca1d3b0477dfcc7e2f09c0494ec148704f974
Nikki-61 <101709424+Nikki-61@users.noreply.github.com>
2022-03-18 19:10:56 +0530

    Add files via upload

---
+++ Mannam Nikitha/module2/StringUppercaseLowercase.java
@@ -0,0 +1,10 @@
+package assignment02;
+
+public class StringUppercaseLowercase {
+	public static void main(String[] args) {
+		String string = "Learning Java Is So Much Fun";
+		System.out.println("Original String : "+string);
+		System.out.println("String to Uppercase : "+string.toUpperCase());
+		System.out.println("String to Lowercase : "+string.toLowerCase());
+	}
+}
