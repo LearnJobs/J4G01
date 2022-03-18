0d02d571c3450c5a6e4bf87a587127edf2f317cc
anangidivya <101708785+anangidivya@users.noreply.github.com>
2022-03-18 13:02:32 +0530

    Add files via upload

---
+++ Anangi Divya/Assignment-3/HandleArithmetic.java
@@ -0,0 +1,14 @@
+package Carry;
+
+public class HandleArithmetic 
+{
+	public static void main(String[] args)
+	{
+		int firstNum=25;
+		int secondNum=20%2;
+		int div=firstNum/secondNum;
+		System.out.println("Result:"+div);
+		System.out.println("After division");
+	}
+
+}
