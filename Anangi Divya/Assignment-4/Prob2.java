0177e71bfedc34515fc852854e1cf72191c22fc7
anangidivya <101708785+anangidivya@users.noreply.github.com>
2022-03-18 16:30:27 +0530

    Add files via upload

---
+++ Anangi Divya/Assignment-4/Prob2.java
@@ -0,0 +1,26 @@
+package Used;
+
+import java.io.*;
+import java.util.*;
+
+public class Prob2//here i create the one class
+{
+	public static void main(String[] args)//this is the main method of the class
+	{
+		HashMap<Integer,String>hm1=new HashMap<>();//here i m using the hashmap(hm) set
+		hm1.put(1, "one");
+		hm1.put(2, "two");
+		hm1.put(3, "three");
+		hm1.put(4, "four");
+		hm1.put(5, "five");
+		
+		//here i print the all vales of hashmap of hm1
+		System.out.println("Mappings of HashMap hm1 are:"+hm1);
+		System.out.println("Mappings of HashMap hm1 are:"+hm1);
+		System.out.println("Mappings of HashMap hm1 are:"+hm1);
+		System.out.println("Mappings of HashMap hm1 are:"+hm1);
+		System.out.println("Mappings of HashMap hm1 are:"+hm1);
+		
+	}
+
+}
