4061e539a6a8766b4eab686c4bb72ee4ddcdf060
anangidivya <101708785+anangidivya@users.noreply.github.com>
2022-03-18 12:55:44 +0530

    Add files via upload

---
+++ Anangi Divya/Assignment-2/Prob5.java
@@ -0,0 +1,28 @@
+package Com.Strings;
+import java.util.Arrays;
+import java.io.*;
+
+//Class
+class prob5 { // here i create one class and my class name is prob5
+
+ public static void main(String[] args)//this is the main method of the class
+ {
+   
+     prob5 t1 = new prob5();//here i create one object and t1 object is created in heap memory
+
+     prob5 t2 = t1;//here i take t2 is object and t1 value is stored in t2
+     System.out.println(t1 == t2);// here  i m using equals method
+     System.out.print(" True if shallow : ");//to print the output
+             
+             
+ }
+}
+
+
+
+
+
+
+
+
+ 
