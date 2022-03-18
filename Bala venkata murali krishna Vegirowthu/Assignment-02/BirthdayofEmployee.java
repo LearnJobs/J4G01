21e4fab3d43f480f602b8f8239ab144665e0d9a1
murali3470 <101712267+murali3470@users.noreply.github.com>
2022-03-18 21:01:42 +0530

    Add files via upload

---
+++ Bala venkata murali krishna Vegirowthu/Assignment-02/BirthdayofEmployee.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class BirthdayofEmployee {
+	public static void main(String args[])
+	{
+		LocalDate date = LocalDate.now();
+		LocalDate date1 = LocalDate.of(1990,04,06);
+		System.out.println(date1);
+		
+		int year= Period.between(date1, date).getYears();
+		System.out.println("age :"+year);
+		
+	}
+
+}
+
