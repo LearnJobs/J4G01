52d87de6a7b32ab0b5e783cb5ad3b88f67ff183a
ValluriDeepa <101707652+ValluriDeepa@users.noreply.github.com>
2022-03-18 21:47:32 +0530

    Add files via upload

---
+++ Valluri Deepa/Assignment-02/Upper.java
@@ -0,0 +1,14 @@
+//module2-3
+
+public class Upper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "This is Dimple";
+		String S1 = new String("myself Deepa");
+		System.out.println(S1.toUpperCase());
+		System.out.println(s.toLowerCase());
+
+	}
+
+}
