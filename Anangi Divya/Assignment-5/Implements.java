dc932957169c00bdca75796992ed757834755ac5
anangidivya <101708785+anangidivya@users.noreply.github.com>
2022-03-20 11:40:23 +0530

    Add files via upload

---
+++ Anangi Divya/Assignment-5/Implements.java
@@ -0,0 +1,12 @@
+package Threads;
+
+public class Implements //here i create a implements class
+{
+	public static void main(String[] args)//this is the main method of the class
+	{
+		Implements t=new Implements();//here i create a obj.
+		
+		System.out.println("Hello World!!");//to print this stmt
+	}
+
+}
