30d43796f5fa81ec7e4c374d0eeb49b4014f5f56
anangidivya <101708785+anangidivya@users.noreply.github.com>
2022-03-20 11:41:36 +0530

    Add files via upload

---
+++ Anangi Divya/Assignment-5/Prob6.java
@@ -0,0 +1,36 @@
+package Threads;
+
+import java.util.ArrayList;
+
+public class Prob6 
+{
+	public static void main(String[] args)
+	{
+
+		String s1= "nani";
+		String s2= "Ma";
+		String s3= "Apple";
+		String s4= "Mummy";
+		String s5= "Man";
+		String s6= "Mat";
+		String s7= "Mouse";
+		String s8= "Sam";
+		String s9= "Mother";
+		String s10= "Manam";
+		
+		
+		System.out.println("String s1 starts with:"+s1.startsWith("M"));
+		System.out.println("String s2 starts with:"+s2.startsWith("M"));
+		System.out.println("String s3 starts with:"+s3.startsWith("M"));
+		System.out.println("String s4 starts with:"+s4.startsWith("M"));
+		System.out.println("String s5 starts with:"+s5.startsWith("M"));
+		System.out.println("String s6 starts with:"+s6.startsWith("M"));
+		System.out.println("String s7 starts with:"+s7.startsWith("M"));
+		System.out.println("String s8 starts with:"+s8.startsWith("M"));
+		System.out.println("String s9 starts with:"+s9.startsWith("M"));
+		System.out.println("String s10 starts with:"+s10.startsWith("M"));
+		
+		
+	}
+	
+}
