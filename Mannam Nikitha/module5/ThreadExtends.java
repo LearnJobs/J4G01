428423016816d13fde3c59c5d107a1b2ca85270b
Nikki-61 <101709424+Nikki-61@users.noreply.github.com>
2022-03-20 15:10:29 +0530

    Add files via upload

---
+++ Mannam Nikitha/module5/ThreadExtends.java
@@ -0,0 +1,11 @@
+package Threads;
+
+public class ThreadExtends//her i create a class
+{
+	public static void main(String[] args)//this is main method of the class
+	{
+			System.out.println("Hello World!!");//to print this stmt
+			Thread t=new Thread();//here i create a one obj.
+			
+		}
+	}
