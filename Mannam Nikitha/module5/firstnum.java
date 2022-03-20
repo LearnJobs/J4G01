fad6a880b5c85636231c81bf94c20750866b6f22
Nikki-61 <101709424+Nikki-61@users.noreply.github.com>
2022-03-20 15:08:49 +0530

    Add files via upload

---
+++ Mannam Nikitha/module5/firstnum.java
@@ -0,0 +1,20 @@
+package Threads;
+
+public class firstnum //here i create a class and my class is firstnum
+{
+	public static void main(String[] args)//this is the main method of the class
+	{
+		int[] a= {22,3,4,2,5,12,21,15,14,16};// here i take integer type of arrray
+		int firstNum=22;//here i take if the first is 22 then
+		if(firstNum>20)//here i m using the if condition .To check the given condition is greater than 20
+		{
+		System.out.println("the value is greater than 20");	//to print the stmt
+		}
+		else
+		{
+			System.out.println("the value is not greater than 20");//or else to print this stmt
+		}
+		
+	}
+
+}
