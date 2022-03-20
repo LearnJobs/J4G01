a9ed5fde30bd444d15cbbb6cf40cf08be020c107
anangidivya <101708785+anangidivya@users.noreply.github.com>
2022-03-20 11:42:18 +0530

    Add files via upload

---
+++ Anangi Divya/Assignment-5/Prob7.java
@@ -0,0 +1,16 @@
+package Threads;
+
+import java.util.ArrayList;
+
+public class Prob7 //here i create a one class and class name is prob7
+{
+	public static void main(String[] args)//this is the main method of the class
+	{
+		String s= "Hi Hello EveryOne";//here i take one value of the string type variable
+		System.out.println(s.startsWith("Hi"));//here to print the stmt and if the stmt is truue or false
+		System.out.println(s.startsWith("One"));//here to print the stmt and if the stmt is truue or false
+		System.out.println(s.startsWith("Hello"));//here to print the stmt and if the stmt is truue or false
+		
+	}
+
+}
