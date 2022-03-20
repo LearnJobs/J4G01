9ba6eafc5f7cccf8f992af0de001f9bdbd246ced
Nikki-61 <101709424+Nikki-61@users.noreply.github.com>
2022-03-20 15:11:34 +0530

    Add files via upload

---
+++ Mannam Nikitha/module5/Problem6.java
@@ -0,0 +1,36 @@
+package Threads;
+
+import java.util.ArrayList;
+
+public class Problem6
+{
+	public static void main(String[] args)
+	{
+
+		String s1= "nikki";
+		String s2= "Madam";
+		String s3= "nice";
+		String s4= "looking";
+		String s5= "naughty";
+		String s6= "nine";
+		String s7= "Mouse";
+		String s8= "rohini";
+		String s9= "nab";
+		String s10= "Mannam";
+		
+		
+		System.out.println("String s1 starts with:"+s1.startsWith("n"));
+		System.out.println("String s2 starts with:"+s2.startsWith("n"));
+		System.out.println("String s3 starts with:"+s3.startsWith("n"));
+		System.out.println("String s4 starts with:"+s4.startsWith("n"));
+		System.out.println("String s5 starts with:"+s5.startsWith("n"));
+		System.out.println("String s6 starts with:"+s6.startsWith("n"));
+		System.out.println("String s7 starts with:"+s7.startsWith("n"));
+		System.out.println("String s8 starts with:"+s8.startsWith("n"));
+		System.out.println("String s9 starts with:"+s9.startsWith("n"));
+		System.out.println("String s10 starts with:"+s10.startsWith("n"));
+		
+		
+	}
+	
+}
