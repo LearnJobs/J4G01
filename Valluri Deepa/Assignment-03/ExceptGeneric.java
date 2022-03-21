816eba202ada5f377651b1d9502b81780c2e98df
ValluriDeepa <101707652+ValluriDeepa@users.noreply.github.com>
2022-03-21 22:08:01 +0530

    Add files via upload

---
+++ Valluri Deepa/Assignment-03/ExceptGeneric.java
@@ -0,0 +1,23 @@
+
+public class ExceptGeneric {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int a[] = new int[7];
+			a[4] = 30 / 0;
+
+			System.out.println("First print statement in try block");
+		} catch (ArithmeticException e) {
+			System.out.println("Warning: ArithmeticException");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Warning: ArrayIndexOutOfBoundsException");
+		} catch (Exception e) {
+			System.out.println("Warning: Some Other exception");
+		}
+		System.out.println("Out of try-catch block...");
+	}
+
+	}
+
+
