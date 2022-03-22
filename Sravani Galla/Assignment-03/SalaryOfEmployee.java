6e9461d760f9c4027429125c810bbcaa8922b088
Sravanigalla <101708752+Sravanigalla@users.noreply.github.com>
2022-03-23 00:11:44 +0530

    Add files via upload

---
+++ Sravani Galla/Assignment-03/SalaryOfEmployee.java
@@ -0,0 +1,28 @@
+package Assignment3;
+import java.util.Scanner;
+public class SalaryOfEmployee {
+
+	public static void main(String[] args) {
+		Scanner Scan = new Scanner(System.in);
+		SalaryOfEmployee emp = new SalaryOfEmployee();
+		System.out.println("EnterId:"+ SetSalaryOfEmployeeId(Scan.nextInt()));
+       System.out.println("EnterName:"+ SetEmployeeName(Scan.nextInt()));
+      System.out.println("EnterSalary:"+ SetEmployeeSalary(Scan.nextdouble()));
+      	System.out.println("enter percentage");
+      	double percentage = Scan.nextDouble(){
+      	static EmployeegetEmployeeDetails() {
+		}
+      	
+      	
+      		public static int get void Percentage() {
+      		}
+      	}
+      		}
+      	}
+	private static void getEmployeeDetails() {
+		
+	}
+
+	}
+}
+}
