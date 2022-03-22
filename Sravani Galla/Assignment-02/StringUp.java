461a3cb630184237f08d0027da9e4fcac62a45cb
Sravanigalla <101708752+Sravanigalla@users.noreply.github.com>
2022-03-22 23:55:31 +0530

    Add files via upload

---
+++ Sravani Galla/Assignment-02/StringUp.java
@@ -0,0 +1,13 @@
+package com.string;
+
+public class StringUp {
+
+	public static void main(String[] args) {
+		String S = new String("Hello");
+		System.out.println(S.toLowerCase());
+		System.out.println(S.toUpperCase());
+		// TODO Auto-generated method stub
+
+	}
+
+}
