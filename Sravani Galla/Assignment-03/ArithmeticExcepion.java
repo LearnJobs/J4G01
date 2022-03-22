01dedae6cecbdd056e749ac7acd6383b9b18a592
Sravanigalla <101708752+Sravanigalla@users.noreply.github.com>
2022-03-23 00:07:05 +0530

    Add files via upload

---
+++ Sravani Galla/Assignment-03/ArithmeticExcepion.java
@@ -0,0 +1,17 @@
+package Assignment3;
+
+public class ArithmeticExcepion {
+
+	public static void main(String[] args) {
+		int firstNum=10;
+		int secondNum = 15% 0;
+			int div = firstNum/secondNum;
+			System.out.println("Result: "+div);
+			
+
+			System.out.println("After division");
+		}
+
+	}
+
+
