469cea835fa6cd8cc5d9d21c703597bf3bc20420
Sunny Dev <98743019+Sunny6478@users.noreply.github.com>
2022-03-22 09:02:21 +0530

    Add files via upload

---
+++ Sunny Dev/Assignment-05/FunctionalInterface04.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class FunctionalInterface04
+{
+
+	public static void main(String[] args) 
+	{
+		List<Integer> arrayList= Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		arrayList.forEach(p->System.out.println("Square of "+p+" is "+p*p));
+	}
+
+}
---
+++ Sunny Dev/Assignment-05/ListSquare08.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListSquare08
+{
+
+	public static void main(String[] args) 
+	{
+		List<Integer> arrayList= Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		List<Integer> arrayStored =  arrayList.stream()
+				.map(a->(a*a))
+				.collect(Collectors.toList());
+		         System.out.println(arrayStored);
+	}
+
+}
