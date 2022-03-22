7e52fac43ec46a39c7dc3cf38ee5870b08c6cd71
Sravanigalla <101708752+Sravanigalla@users.noreply.github.com>
2022-03-22 23:58:23 +0530

    Add files via upload

---
+++ Sravani Galla/Assignment-02/Add2Days.java
@@ -0,0 +1,33 @@
+package com.string;
+
+import java.time.LocalDate;
+
+public class Add2Days {
+
+
+public static void main(String[] args) {
+	java.util.Scanner Scan = new java.util.Scanner(System.in);
+	System.out.println("enter the data of birth:");
+	String input = Scan.nextLine();
+	LocalDate data=LocalDate.parse(input);
+	System.out.println("old age is:");
+}
+public static int CalculateAge(LocalDate dob) {
+	Object local;
+	LocalDate objDate = ((Object) local). Date();
+	if((dob!=null)&&(objDate!=null)) {
+		System.out.println("today is sunday");
+		
+	}
+	else {
+		return 0;
+	}
+	System.out.println("not sunday");
+	}
+	
+
+
+
+
+}
+
