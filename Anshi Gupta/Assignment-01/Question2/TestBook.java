0ed886d3983f59b14982dcae072af38f880347cd
gupta15anshi <101707668+gupta15anshi@users.noreply.github.com>
2022-03-24 22:56:15 +0530

    Add files via upload

---
+++ Anshi Gupta/Assignment-01/Question2/TestBook.java
@@ -0,0 +1,14 @@
+
+public class TestBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book book1=new Book();
+		book1.printBook();
+		Book book2=new Book(102,"pthon","guido van rossum");
+		book2.printBook();
+		Book book3=new Book(103,"c","ansi");
+		book3.printBook();		
+	}
+
+}
