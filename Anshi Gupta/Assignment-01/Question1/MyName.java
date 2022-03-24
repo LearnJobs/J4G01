36ed62f3410b8c2d3e96220c1586e0234c8db62b
gupta15anshi <101707668+gupta15anshi@users.noreply.github.com>
2022-03-24 22:46:27 +0530

    Add files via upload

---
+++ Anshi Gupta/Assignment-01/Question1/MyName.java
@@ -0,0 +1,16 @@
+
+public class MyName {
+	String name;
+	public void nme(String name)
+	{
+		System.out.println("my name is " +name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyName mn =new MyName();
+		mn.nme("Anshi Gupta");
+		//nme("anshi gupta");
+	}
+
+}
