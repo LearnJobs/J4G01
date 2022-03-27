1a4ba0bca6cc005c6bf130fcd046c4e0082bf82d
Sunny Dev <98743019+Sunny6478@users.noreply.github.com>
2022-03-27 11:43:43 +0530

    Add files via upload

---
+++ Sunny Dev/Assignment-05/ExecutorFrameWork03.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorFrameWork03 
+{
+	public static void main(String[] args) 
+	{
+		 ExecutorService service = Executors.newSingleThreadExecutor();
+		 
+		 // using single thread
+		 
+		 service.execute(new Task());
+		 
+		 // we can also use anonymous inner class instead of top level concrete class
+	}
+}
