22e353ef812293366a9dc1aa59eccc408486000d
Sunny Dev <98743019+Sunny6478@users.noreply.github.com>
2022-03-29 17:11:18 +0530

    Update DisplayAllProducts.java

--- Sunny Dev/Assignment-06/DisplayAllProducts.java
+++ Sunny Dev/Assignment-06/DisplayAllProducts.java
@@ -1,29 +1,39 @@
+// Program no 01
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-public class DisplayAllProducts {
 
-	public static void main(String[] args) {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");		//loading a driver i.e. type-4 driver for mysql
+public class DisplayAllProducts 
+{
+
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");		
 			System.out.println("Driver is loaded");
 	
-			// connect to the sql instance and database
+			
+			
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/learnjobs", "root","password");
 			System.out.println("Connection established");
 			
-			Statement statement = connection.createStatement();		//statement object is used to fire the queries
+			Statement statement = connection.createStatement();		
+			
 			ResultSet rs=statement.executeQuery("select * from product");
 			System.out.println("ID    NAME    PRICE    CATEGORY");
 			while(rs.next())
 					System.out.println(rs.getInt(1)+"   "+rs.getString(2)+"   "+rs.getFloat(3)+"   "+rs.getString(4));
 			}
-		catch (ClassNotFoundException e) {
+		catch (ClassNotFoundException e)
+		{
 				System.err.println("Driver not configured");
 		}
-		catch (SQLException e) {
+		catch (SQLException e)
+		{
 				System.err.println("Connection not established");
 		}
 	}
