0536e02e5a51f6f02b6fd17b89ec26588c4a0f47
Bhanusha-NS <101661869+Bhanusha-NS@users.noreply.github.com>
2022-03-29 09:29:48 +0530

    Add files via upload

---
+++ Bhanusha N S/Assignment -06/Main.java
@@ -0,0 +1,21 @@
+package com.assignment06;
+
+import java.util.Scanner;
+
+/**
+ * main method of program no-01 and no-02
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// ProductTable01 producttable = new ProductTable01();
+		// producttable.display_products();
+		Problem02 garments1 = new Problem02();
+		garments1.display_garments("garments");
+
+	}
+
+}
