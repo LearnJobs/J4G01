f1e51d0beffb2edcb6260b2b4ccb947180d2b096
0123Taiseen <36309108+0123Taiseen@users.noreply.github.com>
2022-03-31 10:58:04 -0700

    Update Product.java

--- Shaikh Taiseen Rafik/Assignment-06/Product.java
+++ Shaikh Taiseen Rafik/Assignment-06/Product.java
@@ -13,7 +13,7 @@ public class Product {
 			Connection con=MyConnection.getConnection();
 			System.out.println("Connected to database ");
 			
-			Statement st=con.createStatement();//statement object is used to fire the queries 
+			Statement st=con.createStatement();
 			ResultSet rs=st.executeQuery("select * from product");
 			while(rs.next()) {
 			
