7b6467eb3c71dd85a906b3855d58fb12af7272ed
Bhanusha-NS <101661869+Bhanusha-NS@users.noreply.github.com>
2022-03-31 21:43:11 +0530

    Add files via upload

---
+++ Bhanusha N S/Assignment-07/LoginServlet.java
@@ -0,0 +1,49 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public LoginServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if (username.equals("Bhanusha") && password.equals("77777"))
+			response.getWriter().append("Hello bhanusha");
+		else {
+
+			response.getWriter().append("Invalid Username OR Password...!");
+
+		}
+	}
+
+}
