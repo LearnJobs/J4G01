8b34eccb8aa83fd8ed0d0a8fbe49e0c21361f320
gupta15anshi <101707668+gupta15anshi@users.noreply.github.com>
2022-04-01 00:00:24 +0530

    Add files via upload

---
+++ Anshi Gupta/Assignment4/Q2/QuestionMain2.java
@@ -0,0 +1,23 @@
+package comm.hashmapp;
+
+import java.util.HashMap;
+
+public class QuestionMain2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer,String> pinCode = new HashMap<Integer,String>();
+		pinCode.put(801503,"patna");
+		pinCode.put(411004,"Pune");
+		pinCode.put(223007,"Agra");
+		pinCode.put(700009,"Kolkata");
+		pinCode.put(110001,"New Delhi");
+		pinCode.put(400011,"Mumbai");
+		pinCode.put(281001,"Mathura");
+		pinCode.put(823001,"Gaya");
+		
+		System.out.println(pinCode);
+
+	}
+
+}
