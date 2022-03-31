c3df24472403c0ae64bda5b2a04f05c925e0eede
ValluriDeepa <101707652+ValluriDeepa@users.noreply.github.com>
2022-03-31 23:55:21 +0530

    Add files via upload

---
+++ Valluri Deepa/Assignment-05/Q2.java
@@ -0,0 +1,16 @@
+
+public class Q2 implements Runnable{
+
+	public static void main(String[] args) {
+		Q2 problem2 = new Q2();
+		Thread thread = new Thread(problem2);
+		thread.run();
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Hello World");
+	}
+}
+
+
