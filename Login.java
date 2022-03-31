949298520d9462775391f9caf9f38a489c38aa50
Ashish Kumar Dubey <101744270+ashish123dubey@users.noreply.github.com>
2022-03-31 23:11:15 +0530

    Add files via upload

--- Login.java
+++ Login.java
@@ -1,49 +1,48 @@
-// Write a program to input username and password and check if that combination is available
-//     in the given table.
-
-
-package com.assignment_6;
+package Assignment6;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 
-public class Login {
+public class Login 
+{
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) 
+	{
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver is Loaded");
+				Class.forName("com.mysql.cj.jdbc.Driver");		
+				System.out.println("Driver is loaded");
+	
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Ashish", "root","password");
+				System.out.println("Connection login page");
 			
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/productdb","root","root");
-			System.out.println("Connected to db");
+				Scanner scan=new Scanner(System.in);
+				System.out.print("Input Id : ");
+				String userName=scan.nextLine();
+				System.out.print("Input password : ");
+				String passWord=scan.nextLine();
 			
-			String username = "Siddharth";
-			String password = "siddharth123";
+				String userQuery="SELECT username,password from user WHERE Id=? AND password=?";
 			
-			String query = "Select Username , Password from login where Username= ? AND Password = ?";
-			
-			PreparedStatement pst = con.prepareStatement(query);
-			pst.setString(1, username);
-			pst.setString(2, password);
-			
-			ResultSet rs = pst.executeQuery();
-			
-			if(rs.next()) {
-				System.out.println("Valid Credentials");
-			}else
-				System.out.println("Invalid Credentials");
-			
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			System.out.println("Driver is not Loaded");
-		} catch (SQLException e) {
-			e.printStackTrace();
+				PreparedStatement statement = connection.prepareStatement(userQuery);
+				statement.setString(1, userName);
+				statement.setString(2, passWord); 
+				ResultSet rs=statement.executeQuery();
+				if(rs.next()==false)
+					System.out.println("Invalid Login Credentials");
+				else
+					System.out.println("Login Successfully");
+			}
+		catch (ClassNotFoundException e) 
+		{
+				System.err.println("Driver not configured");
 		}
-
+		catch (SQLException e) {
+				System.err.println("You are not Registered user");
+		}
 	}
-
 }
+
