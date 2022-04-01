49cad444547b1cd41d24a62ddef1971c2f6c08b3
Sravanigalla <101708752+Sravanigalla@users.noreply.github.com>
2022-04-02 00:35:30 +0530

    Add files via upload

---
+++ Sravani Galla/printArrayList.java
@@ -0,0 +1,28 @@
+package Assignment4;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class printArrayList {//here i create one class
+public static void main(String[] args)//this is the main method of the class
+{
+	ArrayList<String>List=new ArrayList<>();//here i create the array list
+	List.add("sravani");
+	List.add("tulasi");
+	List.add("ganesh");
+	List.add("naidu");
+	System.out.println("the list is:"+List);//to print the list
+	Iterator<String> iter=List.iterator();//here i m using the iterator 
+	System.out.println("\n the iterator values"+ List);//to print the stmt
+	while(iter.hasNext()) //here i m using while condition
+	{
+		System.out.println(iter.next()+" ");//to print the stmt
+	}
+	
+}
+	
+}
+
+
+
