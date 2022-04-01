eb864682cede9306e1b09910308ebc37b351a480
Sravanigalla <101708752+Sravanigalla@users.noreply.github.com>
2022-04-02 00:21:11 +0530

    Add files via upload

---
+++ Sravani Galla/Assignment-05/program06.java
@@ -0,0 +1,34 @@
+package Assignment05;
+public class program06 
+{
+	public static void main(String[] args)
+	{
+
+		String s1= "aa";
+		String s2= "bb";
+		String s3= "cc";
+		String s4= "dd";
+		String s5= "ee";
+		String s6= "ff";
+		String s7= "gg";
+		String s8= "hh";
+		String s9= "ii";
+		String s10= "jj";
+		
+		
+		System.out.println("String s1 starts with:"+s1.startsWith("a"));
+		System.out.println("String s2 starts with:"+s2.startsWith("b"));
+		System.out.println("String s3 starts with:"+s3.startsWith("c"));
+		System.out.println("String s4 starts with:"+s4.startsWith("d"));
+		System.out.println("String s5 starts with:"+s5.startsWith("e"));
+		System.out.println("String s6 starts with:"+s6.startsWith("f"));
+		System.out.println("String s7 starts with:"+s7.startsWith("g"));
+		System.out.println("String s8 starts with:"+s8.startsWith("h"));
+		System.out.println("String s9 starts with:"+s9.startsWith("i"));
+		System.out.println("String s10 starts with:"+s10.startsWith("o"));
+		
+		
+	}
+	
+}
+
