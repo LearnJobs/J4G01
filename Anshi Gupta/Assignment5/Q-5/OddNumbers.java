55569594c7bceca2c072c56ada0f9cef2b9a6f46
gupta15anshi <101707668+gupta15anshi@users.noreply.github.com>
2022-04-01 09:31:53 +0530

    Add files via upload

---
+++ Anshi Gupta/Assignment5/Q-5/OddNumbers.java
@@ -0,0 +1,27 @@
+package Question5;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class OddNumbers {
+	public static void main(String[] args) {
+		ArrayList<Integer> numberList = new ArrayList<Integer>();
+		numberList.add(1);
+		numberList.add(2);
+		numberList.add(3);
+		numberList.add(4);
+		numberList.add(5);
+		numberList.add(6);
+		numberList.add(7);
+		numberList.add(8);
+		numberList.add(9);
+		numberList.add(10);
+		Predicate<Integer> oddNumbers = n->n%2!=0;
+		System.out.println("Printing Odd Numbers ");
+		for(Integer number : numberList)
+			if(oddNumbers.test(number))
+				System.out.print(number+" ");
+	}
+
+
+}
