8249fd6626d3bc11c40d5639821bc86f71624bea
Sravanigalla <101708752+Sravanigalla@users.noreply.github.com>
2022-04-06 16:11:39 +0530

    Add files via upload

---
+++ Sravani Galla/Assignment-08/application.java
@@ -0,0 +1,5 @@
+package com.example.demo;spring.jpa.hibernate.ddl-auto=update
+spring.datasource.url=jdbc:mysql://localhost:3306/book
+spring.datasource.username=root
+spring.datasource.password=root
+logging.level.root=WARN
