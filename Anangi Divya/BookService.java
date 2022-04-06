c28c5ec8402b9f7b233b03b1a20f8cbd2ea2a596
anangidivya <101708785+anangidivya@users.noreply.github.com>
2022-04-06 16:18:59 +0530

    Add files via upload

---
+++ Anangi Divya/BookService.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class BookService {
+
+	public Book_class postBook(Book_class book) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Book_class deleteBook(int bookid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
