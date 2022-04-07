8153cf21c7876c2a3f5b20dd3142907884af603d
Rajeshwari05 <101709831+Rajeshwari05@users.noreply.github.com>
2022-04-08 00:02:24 +0530

    Add files via upload

---
+++ Jyoti Awati/Assignment-6/CombinationOfTable.java
@@ -0,0 +1,44 @@
+package Assignment6;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class CombinationOfTable {
+
+	public static void main(String[] args) {
+		try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				System.out.println("Driver is loaded");
+	
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/learnjobs", "root","rajeshwari_55");
+				System.out.println("Connection established");
+			
+				Scanner scan=new Scanner(System.in);
+				System.out.print("Input username : ");
+				String userName=scan.nextLine();
+				System.out.print("Input password : ");
+				String passWord=scan.nextLine();
+			
+				String userQuery="SELECT username,password from user WHERE username=? AND password=?";
+			
+				PreparedStatement statement = connection.prepareStatement(userQuery);
+				statement.setString(1, userName);
+				statement.setString(2, passWord); 
+				ResultSet rs=statement.executeQuery();
+				if(rs.next()==false)
+					System.out.println("Invalid Login Credentials");
+				else
+					System.out.println("Login Successfully");
+			}
+		catch (ClassNotFoundException e) {
+				System.err.println("Driver not configured");
+		}
+		catch (SQLException e) {
+				System.err.println("You are not Registered user");
+		}
+	}
+}
