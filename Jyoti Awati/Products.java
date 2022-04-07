ac7dc8d9bade2dc4e9206dd3452b6271cedde115
Rajeshwari05 <101709831+Rajeshwari05@users.noreply.github.com>
2022-04-08 00:00:01 +0530

    Add files via upload

---
+++ Jyoti Awati/Products.java
@@ -0,0 +1,32 @@
+package Assignment6;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class Products {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");	
+			System.out.println("Driver is loaded");
+	
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/learnjobs", "root","rajeshwari_55");
+			System.out.println("Connection established");
+			
+			Statement statement = connection.createStatement();		
+			ResultSet rs=statement.executeQuery("select * from product");
+			System.out.println("ID    NAME    PRICE    CATEGORY");
+			while(rs.next())
+					System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getFloat(3)+" "+rs.getString(4));
+			}
+		catch (ClassNotFoundException e) {
+				System.err.println("Driver not configured");
+		}
+		catch (SQLException e) {
+				System.err.println("Connection not established");
+		}
+	}
+}
+
